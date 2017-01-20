2eba52ebeac31b4e6d3629a6b555afea4130fe48
John Nowinsky <armthepit@armthepit.com>
2017-01-20 13:03:04 -0600

    update artists schema

--- models/artists.js
+++ models/artists.js
@@ -1,18 +1,87 @@
 var mongoose = require("mongoose");
 
 var artistsSchema = new mongoose.Schema({
-	title: {
+	email: {
 		type: String, 
 	},
-	imgLink: {
+	password: {
 		type: String, 
 	},	
-	storyLink: {
+	name: {
 		type: String, 
 	},
-	summary: {
+	sortName: {
 		type: String, 
+	},	
+	hometown: {
+		type: String, 
+	},	
+	state: {
+		type: String, 
+	},
+	country: {
+		type: String, 
+	},	
+	genre: {
+		type: String, 
+	},	
+	recordLabel: {
+		type: String, 
+	},
+	bio: {
+		type: String, 
+	},	
+	bandMembers: {
+		type: Array, 
+	},	
+	artistWebsite: {
+		type: String, 
+	},
+	facebook: {
+		type: String, 
+	},	
+	reverbnation: {
+		type: String, 
+	},	
+	soundCloud: {
+		type: String, 
+	},	
+	twitter: {
+		type: String, 
+	},
+	youtubeChannel: {
+		type: String, 
+	},	
+	otherWebsite1: {
+		type: String, 
+	},	
+	otherWebsite2: {
+		type: String, 
+	},	
+	representative: {
+		type: String, 
+	},
+	repEmail: {
+		type: String, 
+	},
+	repPhone: {
+		type: String, 
+	},	
+	views: {
+		type: Number, 
+	},	
+	plays: {
+		type: Number, 
 	},		
+	picture: {
+		type: String, 
+	},	
+	songs: {
+		type: Array, 
+	},	
+	youtubeVideo: {
+		type: String, 
+	},	
 	createdAt: {
 		type: Date, 
 		default: Date.now
