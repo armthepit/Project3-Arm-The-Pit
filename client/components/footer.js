082de8ceff8ed253db60911edae793e05e4bf115
Damian Mandola <dmandola11@gmail.com>
2017-01-28 12:55:38 -0600

    added footer.js

---
+++ client/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default () => {
+	return (
+		<nav class="navbar navbar-default navbar-fixed-bottom">
+  			<div class="container">
+    			<h2>&#169;Copyright 2017 Arm The Pit</h2>
+  			</div>
+		</nav>
+	);
+ }
