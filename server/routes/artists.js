81397a05833c98a38a10572bd65857c885995fca
John Nowinsky <armthepit@armthepit.com>
2017-01-29 14:47:01 -0600

    add server side validation

---
+++ server/routes/artists.js
@@ -0,0 +1,45 @@
+ const express = require('express');
+ const Validator = require('validator');
+ const isEmpty = require('lodash/isEmpty');
+
+
+let router = express.Router();
+ 
+ function validateInput(data) {
+   let errors = {};
+ 
+   if (Validator.isEmpty(data.email)) {
+     errors.email = 'This field is required';
+   }
+   if (!Validator.isEmail(data.email)) {
+     errors.email = 'Email is invalid';
+   }
+   if (Validator.isEmpty(data.password)) {
+     errors.password = 'This field is required';
+   }
+   if (Validator.isEmpty(data.passwordConfirmation)) {
+     errors.passwordConfirmation = 'This field is required';
+   }
+   if (!Validator.equals(data.password, data.passwordConfirmation)) {
+     errors.passwordConfirmation = 'Passwords must match';
+   }
+   if (Validator.isEmpty(data.genre)) {
+     errors.genre = 'This field is required';
+   }
+ 
+   return {
+     errors,
+     isValid: isEmpty(errors)
+   }
+ }
+ 
+ router.post('/', (req, res) => {
+   const { errors, isValid } = validateInput(req.body);
+ 
+   if (!isValid) {
+   	console.log(errors);
+     res.status(400).json(errors);
+   }
+ });
+ 
+ module.exports = router;
