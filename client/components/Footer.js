bdb174e40ba555e01de551a5151a8a2c56016873
Damian Mandola <dmandola11@gmail.com>
2017-01-31 19:36:22 -0600

    changed font to Arsenal

---
+++ client/components/Footer.js
@@ -0,0 +1,12 @@
+ import React from 'react';
+ import { Link } from 'react-router';
+ 
+ export default () => {
+  	return (
+  		<nav className="navbar navbar-default navbar-fixed-bottom">
+    			<div className="container">
+     				<h4>&#169;Copyright 2017 Arm The Pit</h4>
+    			</div>
+  		</nav>
+  	);
+  }
