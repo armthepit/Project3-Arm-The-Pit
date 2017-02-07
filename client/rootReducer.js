27d26f1a5a65f7767b4a5edc64dabbb77571827c
John Nowinsky <armthepit@armthepit.com>
2017-02-07 00:42:34 -0600

    add code to show genres

--- client/rootReducer.js
+++ client/rootReducer.js
@@ -4,10 +4,12 @@ import { combineReducers } from 'redux';
  import auth from './reducers/auth';
  import artists from './reducers/artists';
  import countries from './reducers/countries';
+ import genres from './reducers/genres';
  
  export default combineReducers({
    flashMessages,
    auth,
    artists,
-   countries
+   countries,
+   genres
  });
