3915c2b52779f93573938c289c8037cf7e2b51d9
John Nowinsky <armthepit@armthepit.com>
2017-02-06 23:41:04 -0600

    add code to show artists

--- client/rootReducer.js
+++ client/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
  
  import flashMessages from './reducers/flashMessages';
  import auth from './reducers/auth';
+ import artists from './reducers/artists';
  import countries from './reducers/countries';
  
  export default combineReducers({
    flashMessages,
    auth,
+   artists,
    countries
  });
