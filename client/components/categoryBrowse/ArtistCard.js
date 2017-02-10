8e732d62b62ed7a69f358890dfc17183d2357c2d
Damian Mandola <dmandola11@gmail.com>
2017-02-09 20:37:11 -0600

    added content to ArtistCard

--- client/components/categoryBrowse/ArtistCard.js
+++ client/components/categoryBrowse/ArtistCard.js
@@ -8,7 +8,44 @@ export default function ArtistCard({ artist }) {
 				<div className="panel-body">
 					<img src="assets/images/lilgirl.jpg" className="img-rounded img-responsive center-block" alt="{artist.title}" />				
 					<div className="content">
-						{artist.name} {artist.genre}
+						<h4>{artist.name}</h4> 
+						<h7>member since: {artist.createdAt}
+						<h5>Genre: {artist.genre}</h3> 
+						<h5>Hometown: {artist.hometown}, {artist.state}, {artist.country}</h3>
+						<br />
+						<h5>Band Members: {artist.bandmembers}</h3>
+						<h5>Record Label: {artist.recordLabel}</h3>
+						<br />
+						<p>Bio:{artist.bio}</p>
+						<h5>email: {artist.email}</h3>
+						<h3>website: {artist.artistWebsite}</h3>
+						<br />
+						<h5>Representative: {artist.representative}</h3>
+						<h5>Rep email: {artist.repEmail}</h3>
+						<h5>Rep phone: {artist.repPhone}</h3>
+						<br />
+						<h5>songs: {artist.songs}</h3>
+						<h5>plays: {artist.plays}</h3>
+						<h5>views: {artist.views}</h3>
+						<br />
+						<h4>Social Media</h4>
+						<table class="table table-condensed">
+							<tbody>
+						        <tr>
+						            <td>facebook: {artist.facebook}</td>
+						            <td>twitter: {artist.twitter}</td>
+						            <td>soundcloud: {artist.soundcloud}</td>
+						        </tr>
+						        <tr>
+						        	<td>youtube: {artist.youtubeChannel}</td>
+						            <td>reverbnation {artist.reverbnation}</td>
+						            <td>other website: {artist.otherWebsite1}</td>
+						        </tr>
+						        <tr>
+						            <td>other website: {artist.otherWebsite2}</td>
+						        </tr>
+						    </tbody>
+						</table>
 					</div>
 				</div>
 			</div>
