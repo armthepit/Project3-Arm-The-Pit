d105e6501facb3421c2b023dcd843078a43d162c
Damian Mandola <dmandola11@gmail.com>
2017-02-10 13:08:28 -0600

    modified artist page

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -8,11 +8,13 @@
          <h1>Welcome To Arm The Pit</h1>
          <div>
            <h2>Browse Artist By: </h2>
-           <Link to="/browsecountry"><button type="button" className="btn btn-info">COUNTRY</button></Link>
-           <Link to="/browsegenre"><button type="button" className="btn btn-info">GENRE</button></Link>
-           <Link to="/browsename"><button type="button" className="btn btn-info">NAME</button></Link>
-           <Link to="/browsestate"><button type="button" className="btn btn-info">STATE</button></Link>
-        </div>
+           <div className="row">
+               <Link to="/browsecountry"><button type="button" className="btn btn-info col-lg-2 col-md-2 col-sm-2 col-lg-offset-4 col-md-offset-4 col-sm-offset-4">COUNTRY</button></Link>
+               <Link to="/browsegenre"><button type="button" className="btn btn-info col-lg-2 col-md-2 col-sm-2">GENRE</button></Link>
+               <Link to="/browsename"><button type="button" className="btn btn-info col-lg-2 col-md-2 col-sm-2">NAME</button></Link>
+               <Link to="/browsestate"><button type="button" className="btn btn-info col-lg-2 col-md-2 col-sm-2">STATE</button></Link>
+            </div>
+          </div>
        </div>
      );
    }
--- client/components/categoryBrowse/ArtistCard.js
+++ client/components/categoryBrowse/ArtistCard.js
@@ -3,42 +3,39 @@ import { link } from 'react-router';
 
 export default function ArtistCard({ artist }) {
 	return (
-		<div className="col-md-4">
+		<div className="col-md-8 col-md-offset-2">
 			<div className="panel panel-default">
 				<div className="panel-body">
 					<img src="assets/images/lilgirl.jpg" className="img-rounded img-responsive center-block" alt="{artist.title}" />				
 					<div className="content">
 						<h4>{artist.name}</h4> 
-						<h7>member since: {artist.createdAt}</h7>
 						<h5>Genre: {artist.genre}</h5> 
 						<h5>Hometown: {artist.hometown}, {artist.state}, {artist.country}</h5>
 						<br />
-						<h5>Band Members: {artist.bandmembers}</h5>
+						<h5>Band Members: {artist.bandMembers}</h5>
 						<h5>Record Label: {artist.recordLabel}</h5>
 						<br />
 						<p>Bio:{artist.bio}</p>
 						<h5>email: {artist.email}</h5>
 						<h3>website: {artist.artistWebsite}</h3>
 						<br />
-						<h5>Representative: {artist.representative}</h5>
-						<h5>Rep email: {artist.repEmail}</h5>
-						<h5>Rep phone: {artist.repPhone}</h5>
-						<br />
 						<h5>songs: {artist.songs}</h5>
 						<h5>plays: {artist.plays}</h5>
 						<h5>views: {artist.views}</h5>
 						<br />
 						<h4>Social Media</h4>
-						<table class="table table-condensed">
+						<table class="table">
 							<tbody>
 						        <tr>
 						            <td>facebook: {artist.facebook}</td>
 						            <td>twitter: {artist.twitter}</td>
-						            <td>soundcloud: {artist.soundcloud}</td>
 						        </tr>
 						        <tr>
-						        	<td>youtube: {artist.youtubeChannel}</td>
-						            <td>reverbnation {artist.reverbnation}</td>
+						           <td>youtube: {artist.youtubeChannel}</td>
+						           <td>soundcloud: {artist.soundcloud}</td>
+						        </tr>
+						        <tr>
+						        	<td>reverbnation {artist.reverbnation}</td>
 						            <td>other website: {artist.otherWebsite1}</td>
 						        </tr>
 						        <tr>
