a932fa444b85147fde9d04ea6d7dc39e6c05cadf
Drake Ballard <drakeballard@gmail.com>
2017-02-09 19:39:34 -0600

    adding some styling

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -1,6 +1,6 @@
  import React from 'react';
  import { Link } from 'react-router';
-  
+
  class Greetings extends React.Component {
    render() {
      return (
@@ -9,13 +9,13 @@
          <div>
            <h2>Browse Artist By: </h2>
            <Link to="/browsecountry"><button type="button" className="btn btn-info">COUNTRY</button></Link>
-           <Link to="/browsegenre"><button type="button" className="btn btn-info">GENRE</button></Link> 
-           <Link to="/browsename"><button type="button" className="btn btn-info">NAME</button></Link> 
-           <Link to="/browsestate"><button type="button" className="btn btn-info">STATE</button></Link>   
-        </div>     
+           <Link to="/browsegenre"><button type="button" className="btn btn-info">GENRE</button></Link>
+           <Link to="/browsename"><button type="button" className="btn btn-info">NAME</button></Link>
+           <Link to="/browsestate"><button type="button" className="btn btn-info">STATE</button></Link>
+        </div>
        </div>
      );
    }
   }
- 
+
  export default Greetings;
